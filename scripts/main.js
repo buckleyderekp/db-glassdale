e443e1ddc9ce9738ace0e5efaf0d0d6288051105
buckleyderekp <buckleyderekp@gmail.com>
2020-03-16 15:54:09 -0500

    added forms and got notes to work

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ import { CriminalList } from "./criminals/CriminalList.js";
 import { getConvictions, useConvictions } from "./convictions/convictionProvider.js";
 import ConvictionSelect from "./convictions/convictionSelect.js";
 import { getCriminals } from "./criminals/CriminalProvider.js";
+import NoteForm from "./notes/NoteForm.js";
 
 
 
@@ -11,3 +12,5 @@ getCriminals().then(CriminalList)
 
 // first get all convictions, THEN create the conviction dropdown
 getConvictions().then(ConvictionSelect)
+
+NoteForm()
